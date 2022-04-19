3a591dda9aa8474b12fd33244258d35056a89ba0
JacquelineMalatare <jacqueline.malatare@gmail.com>
2022-04-19 13:28:40 -0600

    commit tree ab7c085f23aadc0977dc2552f962122a5cdcdb8d

--- spec/fix_the_string_spec.rb
+++ spec/fix_the_string_spec.rb
@@ -1,8 +1,19 @@
-describe '#fix_the_string' do
-  it 'returns the fixed string' do
-    expect(fix_the_string('')).to eq('')
-    expect(fix_the_string('a')).to eq('a')
-    expect(fix_the_string('cCcOdDdInGGgisSSfUUun')).to eq('cOdInGiSfUn')
-    expect(fix_the_string('efFEFiiIxyeEYeZdDzd')).to eq('Fixed')
+def fix_the_string(str)
+  # we can use an array to stand in for a stack, or implement a stack class
+  stack = []
+
+  # Iterate through the string, adding the "good" characters to the stack
+  str.each_char do |char|
+    if !stack.empty? && char != stack.last && char.upcase == stack.last.upcase
+      stack.pop
+    else
+      stack.push(char)
+    end
   end
+
+  # Pop each element off the stack and add it to the result string
+  result = ''
+  result = stack.pop + result until stack.empty?
+
+  result
 end
